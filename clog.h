e5edfd23c90491aa41a2900d4612abf916df7bbd
pylover <vahid.mardani@gmail.com>
2025-08-22 16:24:55 +0330

    Lint

--- clog.h
+++ clog.h
@@ -1,5 +1,23 @@
-#ifndef CLOG_H
-#define CLOG_H
+// Copyright 2025 Vahid Mardani
+/*
+ * This file is part of clog.
+ *  clog is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  clog is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with clog. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Author: Vahid Mardani <vahid.mardani@gmail.com>
+ */
+#ifndef CLOG_H_
+#define CLOG_H_
 
 
 /* standard */
@@ -118,4 +136,4 @@ clog_dprintf(int fd, enum clog_verbositylevel level, const char *file,
         unsigned int line, const char *func, int flags, const char *fmt, ...);
 
 
-#endif
+#endif  // CLOG_H_
